84005c466fb77ca2ded2ea088748fbf8ce32102c
rrennn08 <66759042+rrennn08@users.noreply.github.com>
2023-04-23 20:56:28 -0700

    tweak to side-to-side air movement

--- Project Vibe/Assets/Scripts/PlayerMovement.cs
+++ Project Vibe/Assets/Scripts/PlayerMovement.cs
@@ -88,7 +88,7 @@ public class PlayerMovement : MonoBehaviour
 
             if(isGrounded == false) 
             {
-                rb.AddForce(0.25f * movement * Vector2.right); //deaccelerate side to side movement when in the air
+                rb.AddForce(0.1f * movement * Vector2.right); //deaccelerate side to side movement when in the air
                 rb.AddForce(5f * Vector2.down); //make them fall quicker in air
             }
             else
