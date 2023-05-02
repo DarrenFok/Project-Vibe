fff4d58c6b7c693b7b51618d1d8d2e2fa3bd186b
rrennn08 <66759042+rrennn08@users.noreply.github.com>
2023-05-02 15:20:12 -0700

    mostly functioning dialogue, just working on typing animation

--- Project Vibe/Assets/Scripts/DialogueManager.cs
+++ Project Vibe/Assets/Scripts/DialogueManager.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro; //text mesh pro
 
 public class DialogueManager : MonoBehaviour
 {
     public Queue<string> sentences;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    public Animator animator; //for animating dialogue box in and out of screen
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +21,11 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with " + dialogue.name);
+        animator.SetBool("IsOpen", true);
+
+        //Debug.Log("Starting conversation with " + dialogue.name);
+
+        nameText.text = dialogue.name;
 
         foreach(string sentence in dialogue.sentences)
         {
@@ -26,14 +37,30 @@ public class DialogueManager : MonoBehaviour
 
     public void DisplayNextSentence()
     {
-        if(sentences.Count == 0)
+        if(sentences.Count == 0) //no sentences left, then end
         {
             EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines(); //in case user goes to next sentence faster than finish animating 
+        StartCoroutine(TypeSentence(sentence));
+
+    }
+
+    IEnumerator TypeSentence (string sentence) //for letters to get "typed" onto screen
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null; //wait a frame to add next letter
         }
     }
 
     void EndDialogue()
     {
-        Debug.Log("End of conversation");
+        animator.SetBool("IsOpen", false);
     }
 }
